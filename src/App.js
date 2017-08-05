2e6cb52639d27b3475bf69aefc5dd19513e9f1eb
hnandago87 <hnandago87@gmail.com>
2017-08-04 20:55:24 -0500

    communication of states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route } from 'react-router-dom';
-import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookList from './BooksList';
 import BookSearch from './BookSearch';
@@ -12,18 +11,20 @@ class BooksApp extends React.Component {
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
+     * showSearchPage: true
+     * 
+     * <Route path='/:id' render={(params)=>(<BookList data={params} books={this.state.books} />)}
+         />
      */
-    showSearchPage: true
+    books:[]
   }
-
   render() {
+    console.log("main app triggered");
     return (
       <div>
-        <Route exact path='/' render={()=>(
-          <BookList />
-        )}
+        <Route exact path='/' render={(params)=>(<BookList />)}
          />
-         <Route path='/search' render={()=>(
+         <Route exact path='/search' render={()=>(
           <BookSearch />
         )}
          /> 
---
+++ src/Read.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+class Read extends Component{
+    
+    render(){
+        console.log("Read is triggered");
+        return (
+            <div className="bookshelf">
+                        <h2 className="bookshelf-title">Read</h2>
+                        <div className="bookshelf-books">
+                            <ol className="books-grid">
+                            {this.props.books.map((book)=>(
+                                <li key={book.id}>
+                                <div className="book">
+                                <div className="book-top">
+                                    <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                                    <div className="book-shelf-changer">
+                                    <select defaultValue={book.shelf} onChange={this.handleChange}>
+                                        <option value="none" disabled>Move to...</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                    </select>
+                                    </div>
+                                </div>
+                                <div className="book-title">{book.title}</div>
+                                <div className="book-authors">{book.author}</div>
+                                </div>
+                            </li>
+                            ))}
+                            </ol>
+                        </div>
+                    </div>
+                        )
+                    }
+}
+export default Read
